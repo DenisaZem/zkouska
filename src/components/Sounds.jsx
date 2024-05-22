0c15e2e51102a187724d4608ca477187dbadd846
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-22 10:43:35 +0200

    Zkouška kodu spouštění_přidání gifu

--- src/components/Sounds.jsx
+++ src/components/Sounds.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { FaPlay, FaPause, FaForward, FaBackward } from 'react-icons/fa';
-import "./Sounds.css"
+import './Sounds.css';
 // Potřeba importovat ikony --> npm install react-icons
 
 const Sounds = () => {
@@ -28,23 +28,50 @@ const Sounds = () => {
     <div className="container">
       <h1>Sounds</h1>
       <audio ref={audioRef} src="/sound/Calm.mp3" />
-     
       <div className="controls">
-      <img 
-         className={`obrazek ${isPlaying ? 'playing' : ''}`}  
-        src="https://media.gettyimages.com/id/1152829238/vector/mindfulness-meditation-and-yoga-background-in-pastel-vintage-colors-with-women-sit-with.jpg?b=1&s=1024x1024&w=gi&k=20&c=_zYSpX6lWyNwsGOtaiDC49YePFSKHot86Z4Ac0mgwwc="
-        alt="obrazek" 
-      />
-      <div className="playButtons">
-        <button onClick={handleBackward} className="control-button backward">
-          <FaBackward />
-        </button>
-        <button onClick={togglePlayPause} className="control-button play-pause">
-          {isPlaying ? <FaPause /> : <FaPlay />}
-        </button>
-        <button onClick={handleForward} className="control-button forward">
-          <FaForward />
-        </button>
+        {/* https://cdn.dribbble.com/users/176039/screenshots/5500538/media/e2b1dd0c440b3fdba253d7d03e0e0874.gif */}
+        <img
+          className={`obrazek ${isPlaying ? 'playing' : ''}`}
+          src="https://media.gettyimages.com/id/1152829238/vector/mindfulness-meditation-and-yoga-background-in-pastel-vintage-colors-with-women-sit-with.jpg?b=1&s=1024x1024&w=gi&k=20&c=_zYSpX6lWyNwsGOtaiDC49YePFSKHot86Z4Ac0mgwwc="
+          alt="obrazek"
+        />
+        <div className="playButtons">
+          <button onClick={handleBackward} className="control-button backward">
+            <FaBackward />
+          </button>
+          <button
+            onClick={togglePlayPause}
+            className="control-button play-pause"
+          >
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </button>
+          <button onClick={handleForward} className="control-button forward">
+            <FaForward />
+          </button>
+        </div>
+      </div>
+
+      <audio ref={audioRef} src="/sound/Calm.mp3" />
+      <p>Na GIF mi ty ikony nejdou napojit zatím :/</p>
+      <div className="controls">
+        <img
+          className={`obrazek ${isPlaying ? 'playing' : ''}`}
+          src="https://cdn.dribbble.com/users/176039/screenshots/5500538/media/e2b1dd0c440b3fdba253d7d03e0e0874.gif"
+          alt="obrazek"
+        />
+        <div className="playButtons">
+          <button onClick={handleBackward} className="control-button backward">
+            <FaBackward />
+          </button>
+          <button
+            onClick={togglePlayPause}
+            className="control-button play-pause"
+          >
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </button>
+          <button onClick={handleForward} className="control-button forward">
+            <FaForward />
+          </button>
         </div>
       </div>
     </div>
